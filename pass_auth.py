c33f1edcbf755e9aac364dfa1894d6e27e74cf3d
Bhargav <bhargavkadali39@gmail.com>
2021-09-12 13:47:34 +0530

    Update pass_auth.py

--- pass_auth.py
+++ pass_auth.py
@@ -1,1 +1,10 @@
-
+import getpass
+database = {"bhargav.kadali": "123456", "kadali.bhargav": "654321"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
